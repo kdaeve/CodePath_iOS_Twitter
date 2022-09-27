433e9c0a4d5ffb83b35d3ef5fa04545282a7006b
DeltaXxx <triple_h@outlook.com>
2022-09-27 14:19:10 -0700

    Complete all features

--- twitter_ios_starter/Twitter/TwitterAPICaller.swift
+++ twitter_ios_starter/Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "4844NexXYDjiJOMqr4bbn0ERM", consumerSecret: "9CaGyOtfZ4z14ZHgFwvKzlKKBPghagex1obOz7s1SeQjluu3yK")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
